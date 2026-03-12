aea993f82777682e97e59f99b4bbfe65fc46e003
Adam Pavel Lipert <a.lipert@protonmail.com>
2026-03-12 11:05:28 +0100

    Add 4 function request using the frankfurter api

--- backend/pkg/api/api.go
+++ backend/pkg/api/api.go
@@ -12,28 +12,88 @@ import (
 	"github.com/joho/godotenv"
 )
 
+func getMinMaxRates(rates map[string]float64) (minKey string, minVal float64, maxKey string, maxVal float64) {
+	firstLoop := true
+
+	for key, value := range rates {
+		if firstLoop {
+			minKey, minVal = key, value
+			maxKey, maxVal = key, value
+			firstLoop = false
+			continue
+		}
+
+		// Check for Strongest (Min)
+		if value < minVal {
+			minVal = value
+			minKey = key
+		}
+
+		// Check for Weakest (Max)
+		if value > maxVal {
+			maxVal = value
+			maxKey = key
+		}
+	}
+	return
+}
+
 type ExchangeApiBaseResponse struct {
 	Base  string             `json:"base"`  // Base currency for reference
 	Date  string             `json:"date"`  // "2026-03-04"
 	Rates map[string]float64 `json:"rates"` // "CZK" : 1.05
 }
 
+type ExchangeApiTimeSeriesResponse struct {
+	Base      string                        `json:"base"`
+	StartDate string                        `json:"start_date"`
+	EndDate   string                        `json:"end_date"`
+	Rates     map[string]map[string]float64 `json:"rates"`
+}
+
 type ExchangeApi interface {
-	GetLatestExchangeNumbers() ExchangeApiBaseResponse
+	GetLatestExchangeNumbers(baseCurrency string) ExchangeApiBaseResponse
+	GetStrongestCurrencyToBase(baseCurrency string) ExchangeApiBaseResponse
+	GetWeakestCurrencyToBase(baseCurrency string) ExchangeApiBaseResponse
+	GetAverageExchangeRateForCurrencies(baseCurrency string, selectedCurrencies string, from string, to string) ExchangeApiTimeSeriesResponse
 }
 
 type ExchangeApiClient struct {
-	baseUrl           string
-	possibleEndpoints map[string]string
+	BaseUrl           string
+	PossibleEndpoints map[string]string
 }
 
-func (e ExchangeApiClient) GetLatestExchangeNumbers() ExchangeApiBaseResponse {
+func (e ExchangeApiClient) GetStrongestCurrencyToBase(baseCurrency string) ExchangeApiBaseResponse {
+	data := e.GetLatestExchangeNumbers(baseCurrency)
+	strongestKey, strongestVal, _, _ := getMinMaxRates(data.Rates)
 
-	fmt.Println("---")
-	fmt.Println(e.baseUrl + e.possibleEndpoints["LATEST"])
-	fmt.Println("---")
+	return ExchangeApiBaseResponse{
+		Base: baseCurrency,
+		Date: data.Date,
+		Rates: map[string]float64{
+			strongestKey: strongestVal,
+		},
+	}
+}
 
-	resp, err := http.Get(e.baseUrl + e.possibleEndpoints["LATEST"])
+func (e ExchangeApiClient) GetWeakestCurrencyToBase(baseCurrency string) ExchangeApiBaseResponse {
+	data := e.GetLatestExchangeNumbers(baseCurrency)
+	_, _, weakestKey, weakestVal := getMinMaxRates(data.Rates)
+
+	return ExchangeApiBaseResponse{
+		Base: baseCurrency,
+		Date: data.Date,
+		Rates: map[string]float64{
+			weakestKey: weakestVal,
+		},
+	}
+}
+
+func (e ExchangeApiClient) GetLatestExchangeNumbers(baseCurrency string) ExchangeApiBaseResponse {
+
+	url := e.BaseUrl + e.PossibleEndpoints["LATEST"] + "?base=" + baseCurrency
+
+	resp, err := http.Get(url)
 
 	if err != nil {
 		panic(err)
@@ -47,6 +107,34 @@ func (e ExchangeApiClient) GetLatestExchangeNumbers() ExchangeApiBaseResponse {
 	}(resp.Body)
 
 	res := new(ExchangeApiBaseResponse)
+
+	err = json.NewDecoder(resp.Body).Decode(res)
+	if err != nil {
+		panic(err)
+	}
+
+	return *res
+}
+
+func (e ExchangeApiClient) GetAverageExchangeRateForCurrencies(baseCurrency string, selectedCurrencies string, from string, to string) ExchangeApiTimeSeriesResponse {
+	url := e.BaseUrl + "/" + from + ".." + to + "?base=" + baseCurrency + "&symbols=" + selectedCurrencies
+
+	fmt.Println(url)
+
+	resp, err := http.Get(url)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			panic(err)
+		}
+	}(resp.Body)
+
+	res := new(ExchangeApiTimeSeriesResponse)
 
 	err = json.NewDecoder(resp.Body).Decode(res)
 	if err != nil {
@@ -71,7 +159,7 @@ func NewExchangeApiClient() ExchangeApi {
 	possibleEndpoints["CURRENCIES"] = currenciesEndpoint
 
 	return &ExchangeApiClient{
-		baseUrl:           baseUrl,
-		possibleEndpoints: possibleEndpoints,
+		BaseUrl:           baseUrl,
+		PossibleEndpoints: possibleEndpoints,
 	}
 }
